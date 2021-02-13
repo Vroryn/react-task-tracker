f78f4f15dc5cd8b9a27e9ef1a61717a1429fc3da
Vroryn <vroryn@gmail.com>
2021-02-12 17:30:26 -0800

    Add toggle

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 import { useState } from 'react'
 
 const App = () => {
+
+  const [showAddTask, setShowAddTask] = useState (false)
 
   const[tasks, setTasks] = useState(
     [
@@ -29,6 +32,15 @@ const App = () => {
     
     ])
 
+    //Add Task
+
+    const addTask = (task) => {
+      const id = Math.floor(Math.random() * 10000)+1
+
+      const newTask = {id, ... task}
+      setTasks([...tasks, newTask])
+    }
+
     // Delete task
 
     const deleteTask = (id) => {
@@ -44,7 +56,8 @@ const App = () => {
 
   return (
     <div className='container'>     
-      <Header />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : "No Tasks to Show"}
     </div>
   );
